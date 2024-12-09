ea0de06a5203dd523c2a2c8c19a02edff0a65708
Ocheezyy <miniod2018@gmail.com>
2024-12-09 18:50:31 -0500

    Spacing cleanup in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import { ClerkProvider } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
+
 import { BrowserRouter } from "react-router";
+import AppRoutes from "@/routes";
+
 import { Provider as ReduxProvider } from "react-redux";
 import store from "@/store.ts";
-import AppRoutes from "@/routes";
+
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
