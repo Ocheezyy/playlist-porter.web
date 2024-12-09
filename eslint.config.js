8d66b42332a5d30fa9fa506458deb3df2903ff89
Ocheezyy <miniod2018@gmail.com>
2024-12-09 12:03:42 -0500

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import stylistic from "@stylistic/eslint-plugin-ts";
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      "@stylistic/ts": stylistic,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "@stylistic/ts/semi": ["error", "always"],
+      "@stylistic/ts/object-curly-spacing": ["error", "always"],
+      "@stylistic/ts/quotes": ["error", "double"]
+    },
+  },
+)
