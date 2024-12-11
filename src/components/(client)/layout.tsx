1e69198224ce42befd21fb37607f94a76a908edb
ocheezyyy <seanodonnellse@gmail.com>
2024-12-10 21:53:05 -0500

    Added retrieval of access token from query params when spotify authentication is complete.

--- src/components/(client)/layout.tsx
+++ src/components/(client)/layout.tsx
@@ -4,20 +4,29 @@ import { AppSidebar } from "./app-sidebar";
 import { Outlet } from "react-router";
 import { useDispatch } from "react-redux";
 import { setClerkToken } from "@/features/auth/auth-reducer.ts";
+import { setToken as setSpotifyToken } from "@/features/spotify/spotify-reducer.ts";
 import { useEffect } from "react";
+import queryString from "query-string";
+import { fetchSpotifyPlaylists } from "@/features/spotify/spotify-actions.ts";
 
 
 export default function ClientLayout() {
-    const { getToken } = useAuth();
+    const { getToken: getClerkToken } = useAuth();
     const dispatch = useDispatch();
 
-    async function setToken() {
-        const token = await getToken();
+    async function setClerkTokenC() {
+        const token = await getClerkToken();
         dispatch(setClerkToken(token));
     }
 
     useEffect(() => {
-        setToken();
+        setClerkTokenC();
+
+        const parsed = queryString.parse(window.location.search);
+        if (parsed?.access_token) {
+            dispatch(setSpotifyToken(parsed.access_token));
+            fetchSpotifyPlaylists(parsed.access_token as string);
+        }
     }, []);
 
     return (
